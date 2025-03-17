1a36e5d0f23724333e80fd92c45387ce6f716272
Pronoy Das <pronoysunny.47@gmail.com>
2025-03-17 17:20:08 +0530

    Add JS code for Upload file to AWS

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -3,6 +3,14 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+// Get input from action.yml file
+    const bucketName=core.getInput('bucket' ,{required: true});
+    const bucketRegion=core.getInput('bucket' ,{required: false});
+    const distFolder=core.getInput('dist-folder' ,{required: true});
+// Upload File
+    const s3Uri = `s3://${bucketName}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+// Printing text into github runner mc console
    core.notice("hello Its  from JS custom msg::")
 
 }
