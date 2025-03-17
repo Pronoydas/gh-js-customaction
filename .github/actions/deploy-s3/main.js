e6edb6bd85f4598c8f79794c9b7ed9f2613bd067
Pronoy Das <pronoysunny.47@gmail.com>
2025-03-17 15:43:09 +0530

    Added JS Custom Actions

---
+++ .github/actions/deploy-s3/main.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+function run() {
+   core.notice("hello Its  from JS custom msg::")
+
+}
+
+run();
